a29ba60aa556e0f6f33f9834cf64f553ea23f1bf
maxie-netizen <maxwellirungu64@gmail.com>
2025-05-28 09:26:51 +0000

    Initial commit

---
+++ system/config.js
@@ -0,0 +1,21 @@
+/*
+┏━━━━━━━━━━━━━━━┓  
+┃ RIKZZ BASE - WHATSAPP     
+┣━━━━━━━━━━━━━━━┛
+┃♕ Creator: kayzen           
+┃♕ AI Helper: ChatGPT             
+┃♔ Version: 1.0.0                   
+┗━━━━━━━━━━━━━━━┛
+*/
+//========RIKZZ========
+global.prefix = [".", "!", ".", ",", "🐤", "🗿"]; 
+global.publik = true
+global.owner = ["62882007195252"] 
+global.namabot = 'Kayzen V1'
+//======================
+global.mess = { 
+owner: 'owner only!',
+premium: 'anda bukan user premium',
+succes: 'succes'
+}
+//======================
